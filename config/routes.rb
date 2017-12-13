5673a3cd933c4223a9b1f478c2dc0c640ad73ac7
palmierie <ericmpalmieri@gmail.com>
2017-12-12 19:16:50 -0600

    adding dashboard

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
-  root :to => 'users#index'
+  root :to => 'pages#home'
   resources :user_sessions
   resources :users
-  
+
+  get 'home', to: 'pages#home', as: 'home'
+  get 'dashboard', to: 'pages#dashboard', as: 'dashboard'
   get 'login' => 'user_sessions#new', :as => :login
   post 'logout' => 'user_sessions#destroy', :as => :logout
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
