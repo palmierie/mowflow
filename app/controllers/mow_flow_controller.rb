ab94031878d29ffbd4e0d4e9b12ed1d41303f414
palmierie <ericmpalmieri@gmail.com>
2017-12-20 14:43:07 -0600

    ready for python algo

--- app/controllers/mow_flow_controller.rb
+++ app/controllers/mow_flow_controller.rb
@@ -7,6 +7,10 @@ class MowFlowController < ApplicationController
   end
   
   def optimize_list
+    # get depot
+    @user_business = UserBusiness.where('user_id = ?', current_user).first
+    @business = Business.where('id = ?', @user_business.business_id).first
+    @depot = ScheduledLocation.where('business_id = ? AND depot = ?', @business.id, true).first.as_json
     # get number of routes (days) for optimization
     @number_of_routes = params["days"].to_i
     # get range of dates to for query
@@ -14,8 +18,8 @@ class MowFlowController < ApplicationController
     @dates = Date.today..(Date.today + @number_of_days)
     # get jobs from database that are equal to the date range
     @jobs = ScheduledLocation.where(:next_mow_date => @dates).as_json
-    @depot = ScheduledLocation.where(:next_mow_date => @dates).as_json
-
+    # add the depot to the beginning of the array
+    @jobs.unshift(@depot)
     # arrange job IDs in array
     @location_ids = []
     # arrange job durations in array
@@ -30,17 +34,28 @@ class MowFlowController < ApplicationController
     end
     @coordinates_string = @coordinates_string.chop!
 
-    puts "coordinates: #{@coordinates_string}"
-    puts "location ids: #{@location_ids}"
-    puts "duration_per_location: #{@duration_per_location}"
-    puts "number of routes: #{@number_of_routes}"
+    @location_matrix = create_matrix(@coordinates_string)
 
-    coordString = "-86.9116559,40.461655;-86.921173,40.46331199999999;-86.92182,40.46331;-86.90959199999999,40.4673048;-86.9364658,40.4795477;-86.97491,40.475311;-86.90780629999999,40.4946867;-86.90099339999999,40.47090149999999;-86.92161779999999,40.45996969999999;-86.92226699999999,40.450081;-86.9229952,40.4576658;-86.92029099999999,40.463401;-86.921531,40.463736;-86.9042097,40.4640966;-86.9121128,40.5065488;-86.922682,40.46330700000001;-86.91655659999999,40.448682;-86.9032374,40.4725059;-86.9030104,40.47211"
-    @matrix = ApiCalls.get_matrix(@coordinates_string)
-    puts "matrix: #{@matrix}"
-    puts "after api call"
+    @data_hash_for_optimization = {
+                                    location_ids: @location_ids,
+                                    location_matrix: @location_matrix,
+                                    duration_per_location: @duration_per_location,
+                                    number_of_routes: @number_of_routes 
+                                  }
+    @opto_data = optimize_mine(@data_hash_for_optimization)
+                              
   end
 
+  private
 
+    def create_matrix(coord_string)
+      @matrix = ApiCalls.get_matrix(coord_string)
+      return @matrix
+    end
+
+    def optimize_mine(data_hash)
+      
+      return @opto_data
+    end
 
 end
