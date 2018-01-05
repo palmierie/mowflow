94f1a3f14702463461151928847a4b1bdd9401f0
palmierie <ericmpalmieri@gmail.com>
2018-01-05 16:00:12 -0600

    added error handling for OSRM API and Google OR-Tools

--- lib/api_calls.rb
+++ lib/api_calls.rb
@@ -20,7 +20,14 @@ class ApiCalls
     # 100 coordinate max limit from OSRM
     base_uri 'http://router.project-osrm.org'
     @response = get("/table/v1/driving/#{coordinates_string}")
-    @durations = @response["durations"]
+    @durations = []
+    if @response["code"] == "Ok"
+      @durations[0] = "ok"
+      @durations[1] = @response["durations"]
+    else  
+      @durations[0] = "bad"
+      @durations[1] = @response["message"]
+    end
     return @durations
   end
 
@@ -28,7 +35,6 @@ class ApiCalls
     json = JSON.dump(data_hash)
     
     # block form
-    # Open3.popen3("python ./ortools_d/examples/python/delete_me.py"){|stdin, stdout, stderr, wait_thr|
     Open3.popen3("python #{ENV['PYTHONPATH']}/lib/ortools_d/examples/python/cvrptw_mowflow.py"){|stdin, stdout, stderr, wait_thr|
       pid = wait_thr[:pid]
       stdin.write(json)
@@ -36,7 +42,11 @@ class ApiCalls
       stderr.close()
       exit_status = wait_thr.value
       pythondata = stdout.read()
-      json = JSON.parse(pythondata)
+      if pythondata == ""
+        json = "Error with Google OR-Tools Script"
+      else
+        json = JSON.parse(pythondata)
+      end
     }
     return json
   end
